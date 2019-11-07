548237e8a6038afdd3878fde622476873cd86558
imranjeetsingh <ranjee970@gmail.com>
2019-11-07 09:29:50 +0530

    added applyMiddleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 
 import './index.css';
@@ -9,12 +9,23 @@ import registerServiceWorker from './registerServiceWorker';
 import CounterReducer from './store/reducers/counter';
 import StoreReducer from './store/reducers/Result';
 
+const logger = store =>{
+    return next =>{
+        return action =>{
+            console.log('Middleware dispatching', action, store)
+            const result = next(action)
+            console.log('middleware next state', store.getState())
+            return result
+        }
+    }
+}
+
 const reducer = combineReducers({
     ctr : CounterReducer,
     res : StoreReducer
 })
 
-const store = createStore(reducer);
+const store = createStore(reducer,applyMiddleware(logger));
 
 ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
